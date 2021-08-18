6307931d9347445620c1d7de52976156fa85225d
JonathanCout <jqcoutinho.eng@gmail.com>
2021-08-18 15:58:12 -0300

    adicionando exercicio aula05

---
+++ Aula05/src/Carro.java
@@ -0,0 +1,19 @@
+public class Carro {
+    private int ano;
+    private String placa;
+    private String chassi;
+    private Integer kmPercorrido = 0;
+    private boolean garantia = true;
+
+        public Carro (int ano, String chassi) {
+            this.ano = ano;
+            this.chassi = chassi
+        }
+        public int getAno() {
+            return ano;
+        }
+        
+
+
+    }
+
